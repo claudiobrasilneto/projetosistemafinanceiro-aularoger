0f89167f47afd3a92e694c46db65219c4cf98ddd
claudiobrasilneto <claudionetopro@gmail.com>
2022-01-11 14:07:10 -0300

    Array de transações e função de verificação receita ou despesa no amount

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const dummyTransactions = [
+    { id: 1, name: 'bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'torta', amount: -20 },
+    { id: 4, name: 'bolo de brigadeiro', amount: -20 }
+    
+]
+
+const addTransactionIntoBOM = transaction => {
+ const operator = transaction.amount < 0 ? '-' : '+'
+ console.log(operator)
+   /* <li class="minus">
+        Salário <span>-$400</span><button class="delete-btn">x</button</button>
+    </li> */
+}
 
 
+
+addTransactionIntoBOM(dummyTransactions[1])
