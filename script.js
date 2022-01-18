a606530a0ac854f8b5d44e845892edce21ed257d
claudiobrasilneto <claudionetopro@gmail.com>
2022-01-17 22:22:57 -0300

    Botão de remover

--- script.js
+++ script.js
@@ -7,45 +7,54 @@ const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
 
-const dummyTransactions = [
+let dummyTransactions = [
     { id: 1, name: 'bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Salário', amount: 300 },
     { id: 3, name: 'torta', amount: -20 },
     { id: 4, name: 'Violão', amount: 150 }
-    
+
 ]
 
-const addTransactionIntoDOM = transaction => {
-const operator = transaction.amount < 0 ? '-' : '+'
-const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
-const amountWithoutOperator = Math.abs(transaction.amount)
-const li = document.createElement('li')
+const removeTransaction = ID => {
+   dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID) 
+   init()
+}
 
-li.classList.add(CSSClass)
-li.innerHTML = `
-${transaction.name} <span> ${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+${transaction.name} 
+<span> ${operator} R$ ${amountWithoutOperator}</span>
+<button class="delete-btn" onClick="removeTransaction(${transaction.id})">
+x
+</button>
 `
-transactionsUl.append(li)
+    transactionsUl.append(li)
 }
 
 // Balanaços e total
 const updateBalanceValues = () => {
     const transactionsAmounts = dummyTransactions
-    .map(transaction => transaction.amount) 
+        .map(transaction => transaction.amount)
     const total = transactionsAmounts
-    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
     const income = transactionsAmounts
-    .filter(value => value > 0)
-    .reduce((accumulator, value) => accumulator + value, 0)
-    .toFixed(2)
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
     const expense = Math.abs(transactionsAmounts
-    .filter(value => value < 0)
-    .reduce((accumulator, value) => accumulator + value, 0))
-    .toFixed(2)
-  
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+
     balanceDisplay.textContent = `R$ ${total}`
     incomeDisplay.textContent = `R$ ${income}`
-    expenseDisplay.textContent = `R$ ${expense}` 
+    expenseDisplay.textContent = `R$ ${expense}`
 
 }
 
@@ -56,7 +65,7 @@ const init = () => {
     updateBalanceValues()
 }
 
-init ()
+init()
 //const geradora de ID aleatório
 const generateID = () => Math.round(Math.random() * 1000)
 
@@ -73,16 +82,16 @@ form.addEventListener('submit', event => {
         return
     }
 
-    const transaction = { 
-        id: generateID(), 
-        name: transactionName, 
-        amount: Number(transactionAmount) 
+    const transaction = {
+        id: generateID(),
+        name: transactionName,
+        amount: Number(transactionAmount)
     }
 
     dummyTransactions.push(transaction)
-   
- init()
-inputTransactionName.value = ""
- inputTransactionAmount.value = ""
-  
+
+    init()
+    inputTransactionName.value = ""
+    inputTransactionAmount.value = ""
+
 })
